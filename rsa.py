ca910cbbd90eb68dd848c98aeeb66f37a7d75b59
Y0a0bon <collsebastien1993@gmail.com>
2017-01-29 21:08:34 +0100

    Update 1.6.1
    Added Wiener attack

--- rsa.py
+++ rsa.py
@@ -174,8 +174,102 @@ def broadcast_attack_old(m, e, n, verbose):
         a=0
 
 
+#
+# Wiener attack
+# Source : ndiab
+#
+def Wiener(N : int, e : int) -> int :
+        """
+        Wiener Attack
+        return the private key if it's vulnerable
+        if not, return 0
+        """
+        
+        #1 - initiate the list of partial quotients
+        quot = utils.cont_frac(e,N)
+        
+        #2 - initiate a list of continued fractions
+        seq = utils.frac_seq(quot)
+
+        #check if d is actually the key
+        for (k,d) in seq :
+                # Now we are going to check if the couple (k,d)
+                # produce a factorization of N (p * q)
+                
+                if k == 0:
+                        continue
+
+                # compute phi
+                # phi(N) = (ed - 1)/ k
+                phi = (e * d - 1) // k
+                
+                # Now from phi and N we can deduce the eventual factorization
+                # by solving the equation : x^2 - ((N-phi)+1)x + N
+                a = 1
+                b = (N - phi) + 1
+                c = N
+                
+                discr = b*b - 4 * a * c
+        
+                # in that case we got solutions to the equation
+                if discr > 0 :
+                        # to works, the solutions must be integers
+                        # first, the root of the discriminent must be integer 
+                        rdiscr = utils.isqrt(discr)
+
+                        if (rdiscr * rdiscr) == discr :
+                                #the root of the discr is integer, we compute the solutions 
+                                p = -((-b - rdiscr) // (2 * a))
+                                q = -((-b + rdiscr) // (2 * a))
+                                if N == (p * q) :
+                                        print("Hacked")
+                                        return d
+
+        print("Not vulnerable to Wiener")
+
+        return 0
 
 
+#
+# Attack on exo2
+# Source : ndiab
+#
+def attack_exo_2(N : int, e : int, N_bis : int, e_bis : int) -> int :
+	d = Wiener (N,e)
+	nblb = int(log2(d))  # number of least significant bits k known
+
+	offset = 1 << (nblb +1)	 # Offset for each iteration
+	max_iter = 1 << (276 - nblb)
+
+	print(max_iter)
+	print(offset)
+
+	d_bis = d + offset
+
+	m_ref = randint(10,1000)
+
+	m = [m_ref,N_bis]
+	
+	# brute force the high order bits
+	for i in range (max_iter):
+		# to check if the key works, we compare the encryption and the decryption	
+		
+		if i%100000 == 0 :
+			print (i)
+	 
+		m[0] = fast_exp(m[0], e_bis, m[1], 0)
+		m[0] = fast_exp(m[0], d_bis, m[1], 0)		
+		if (m[0] == m_ref) :
+			print("Hacked")
+			print(d_bis)
+			return d_bis
+
+
+		d_bis = d_bis + offset
+		m[0] = m_ref
+
+	print("doesn't work")
+	return 0
     
 
 
